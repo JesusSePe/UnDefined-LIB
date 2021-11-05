336f59042ea3fbe2977309169af44fcc88299b2a
ladyangel1981 <angiemarie1981@hotmail.com>
2021-11-05 20:10:04 +0100

    Uploading ddbb lib - hibernate

---
+++ src/main/java/objects/User.java
@@ -0,0 +1,48 @@
+package objects;
+
+public class User {
+	private long userId;
+	private String username;
+	private String email;
+	private String password;
+	
+	public User(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
